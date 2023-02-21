c11c07a763c77790526b818951f982d4772a4804
Vaneet Chanana <vaneetchanana98@gmail.com>
2023-02-21 23:50:30 +0530

    trying git testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,13 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+
+  //with render() we are creating a virtual dom 
   render(<App />);
+
+  //with screen here we are looking for an element with the text learn react
   const linkElement = screen.getByText(/learn react/i);
+
+  //with jest expect() method we are looking if the link element is in the document with the matcher function 
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/components/greet.js
@@ -0,0 +1,5 @@
+export default function greet() {
+  return (
+    <div>Hello</div>
+  )
+}
